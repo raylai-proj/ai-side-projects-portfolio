a826cfcc44bee9c30be56c56e05367a101162c7e
raylai <chunjueilai@gmail.com>
2025-09-04 20:05:37 +0800

    chore: initialize churn-prediction

---
+++ churn-prediction/src/churn_prediction/main.py
@@ -0,0 +1,12 @@
+"""
+Summary: app entry point for churn-prediction.
+
+Name: Chun-Juei Lai
+Email: chunjueilai@gmail.com
+Date: 9/4/2025
+"""
+
+
+def main():
+    """Churn-prediction entry point."""
+    print("Hello World")
